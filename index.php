cb5d2fd4c85a472fa26e282bfb44220465adbb2e
hemantsingh345 <37499746+hemantsingh345@users.noreply.github.com>
2020-10-09 21:39:36 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+ <head>
+  <link rel="stylesheet" type="text/css" href="css/decoration.css" />
+  <script src="js/formvalidate.js"></script>  
+  <title>Student menagement system</title>
+ </head>
+ <body>
+     <?php 
+	   include('include/connection.php');
+	 ?>
+     <?php 
+	   include('include/header.php');
+	 ?> 
+
+	   <div class="container" >
+	      
+	   </div>
+	 <?php   
+	   include('include/footer.php');
+	 ?>
+ </body>
+</html>
